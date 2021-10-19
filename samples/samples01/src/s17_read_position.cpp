d40a637ef2640dbe94f767f8923e7cc4245db89b
ShotaAk <s.aoki@rt-net.jp>
2021-10-19 19:39:10 +0900

    s17の胴体グループ名をtorsoに変更

--- samples/samples01/src/s17_read_position.cpp
+++ samples/samples01/src/s17_read_position.cpp
@@ -35,21 +35,36 @@ int main()
       break;
     }
 
+    if(!hardware.sync_read("left_arm")){
+      std::cerr<<"left_armグループのsync readに失敗しました."<<std::endl;
+      break;
+    }
+
+    if(!hardware.sync_read("left_hand")){
+      std::cerr<<"left_handグループのsync readに失敗しました."<<std::endl;
+      break;
+    }
+
+    if(!hardware.sync_read("torso")){
+      std::cerr<<"torsoグループのsync readに失敗しました."<<std::endl;
+      break;
+    }
+
     double position;
     int dxl_id = 2;
     if(hardware.get_position(dxl_id, position)){
       std::cout<<"ID:"<<std::to_string(dxl_id)<<"のサーボ角度は"<<std::to_string(position)<<"radです."<<std::endl;
     }
 
-    std::string joint_name = "right_arm_joint_hand";
+    std::string joint_name = "left_arm_joint_hand";
     if(hardware.get_position(joint_name, position)){
       std::cout<<joint_name<<"のサーボ角度は"<<std::to_string(position)<<"radです."<<std::endl;
     }
 
     std::vector<double> positions;
-    if(hardware.get_positions("right_arm", positions)){
+    if(hardware.get_positions("torso", positions)){
       for(int i=0; i<positions.size(); i++){
-        std::cout<<"right_armグループの"<<std::to_string(i)<<"番目のサーボ角度は"<<std::to_string(positions[i])<<"radです."<<std::endl;
+        std::cout<<"torsoグループの"<<std::to_string(i)<<"番目のサーボ角度は"<<std::to_string(positions[i])<<"radです."<<std::endl;
       }
     }
 
