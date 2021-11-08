4112574f4d79d8624b4c7ad8ffc5428867c0b446
ShotaAk <s.aoki@rt-net.jp>
2021-11-08 16:29:04 +0900

    x7_write_velocityを修正

--- samples/samples01/src/x7_write_velocity.cpp
+++ samples/samples01/src/x7_write_velocity.cpp
@@ -20,7 +20,7 @@
 #include "rt_manipulators_cpp/hardware.hpp"
 
 int main() {
-  std::cout << "CRANE-X7のサーボモータ目標速度を書き込むサンプルです." << std::endl;
+  std::cout << "CRANE-X7のサーボモータに目標速度を書き込むサンプルです." << std::endl;
 
   std::string port_name = "/dev/ttyUSB0";
   int baudrate = 3000000;  // 3Mbps
@@ -49,25 +49,30 @@ int main() {
     return -1;
   }
 
+  std::cout << "5秒後に手先が動き出すため、手先の周りに物や人を近づけないで下さい." << std::endl;
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+
   // 目標速度を段階的に早くする
   const double MAX_VELOCITY = M_PI;
   const double STEPS = 10;
-
-  for(int i=0; i<STEPS; i++){
-    double goal_velocity = M_PI * i / static_cast<double>(STEPS);
-    std::cout << "goal_velocity:" << goal_velocity << " rad/s" << std::endl;
+  for (int i=0; i < STEPS; i++) {
+    double goal_velocity = MAX_VELOCITY * i / static_cast<double>(STEPS);
+    std::cout << "set velocity:" << goal_velocity << " rad/s" << std::endl;
     hardware.set_velocity(6, goal_velocity);
     hardware.set_velocity(7, goal_velocity);
     hardware.set_velocity(8, goal_velocity);
     std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "set velocity:" << -goal_velocity << " rad/s" << std::endl;
     hardware.set_velocity("joint5", -goal_velocity);
     hardware.set_velocity("joint6", -goal_velocity);
     hardware.set_velocity("joint7", -goal_velocity);
     std::this_thread::sleep_for(std::chrono::seconds(1));
   }
 
+  std::cout << "set velocity: 0.0 rad/s" << std::endl;
   std::vector<double> goal_velocities = {0.0, 0.0, 0.0};
   hardware.set_velocities("forearm", goal_velocities);
+  std::this_thread::sleep_for(std::chrono::seconds(1));
 
   std::cout << "スレッドを停止します." << std::endl;
   hardware.stop_thread();
