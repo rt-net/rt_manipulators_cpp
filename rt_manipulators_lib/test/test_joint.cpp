4915978d3f540366a4da0f831a377e76e448cdb2
ShotaAk <s.aoki@rt-net.jp>
2021-12-06 17:16:30 +0900

    jointクラスの初期化テストを追加

--- rt_manipulators_lib/test/test_joint.cpp
+++ rt_manipulators_lib/test/test_joint.cpp
@@ -12,18 +12,46 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <memory>
+
 #include "gtest/gtest.h"
 #include "rt_manipulators_cpp/joint.hpp"
 
+class JointTestFixture : public ::testing::Test {
+protected:
+  virtual void SetUp() {
+    uint8_t id = 0;
+    uint8_t operating_mode = 1;
+    double min_position_limit = 2.0;
+    double max_position_limit = 3.0;
+    double current_limit_when_position_exceeds_limit = 4.0;
+    test_joint = std::make_shared<joint::Joint>(
+      id, operating_mode, max_position_limit, min_position_limit,
+      current_limit_when_position_exceeds_limit);
+  };
+  virtual void TearDown() {
+    test_joint.reset();
+  };
 
-TEST(JointTest, initialize_id) {
-  uint8_t id = 0;
-  uint8_t operating_mode = 1;
-  double min_position_limit = 2.0;
-  double max_position_limit = 3.0;
-  double current_limit_when_position_exceeds_limit = 3.0;
-  joint::Joint test_joint(
-    id, operating_mode, max_position_limit, min_position_limit,
-    current_limit_when_position_exceeds_limit);
-  EXPECT_EQ(test_joint.id(), 0);
+  std::shared_ptr<joint::Joint> test_joint;
+};
+
+TEST_F(JointTestFixture, initialize_id) {
+  EXPECT_EQ(test_joint->id(), 0);
+}
+
+TEST_F(JointTestFixture, initialize_operating_mode) {
+  EXPECT_EQ(test_joint->operating_mode(), 1);
+}
+
+TEST_F(JointTestFixture, initialize_min_position_limit) {
+  EXPECT_DOUBLE_EQ(test_joint->min_position_limit(), 2.0);
+}
+
+TEST_F(JointTestFixture, initialize_max_position_limit) {
+  EXPECT_DOUBLE_EQ(test_joint->max_position_limit(), 3.0);
+}
+
+TEST_F(JointTestFixture, initialize_current_limit) {
+  EXPECT_DOUBLE_EQ(test_joint->current_limit_when_position_exceeds_limit(), 4.0);
 }
