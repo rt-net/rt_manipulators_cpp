0aa5ec4fbca57d60942645726882e5cfd29a0283
ShotaAk <s.aoki@rt-net.jp>
2022-02-02 12:10:45 +0900

    samples03をcpplintでチェックする

--- .tools/cpplint.bash
+++ .tools/cpplint.bash
@@ -6,6 +6,7 @@ SCRIPT_DIR=$(dirname $0)
 LIB_DIR=$SCRIPT_DIR/../rt_manipulators_lib
 SAMPLES01_DIR=$SCRIPT_DIR/../samples/samples01
 SAMPLES02_DIR=$SCRIPT_DIR/../samples/samples02
+SAMPLES03_DIR=$SCRIPT_DIR/../samples/samples03
 
 function cpplint_check () {
     cpplint --filter=-build/c++11,-runtime/reference --linelength=100 --extensions=hpp,cpp $1/include/* $1/src/* $1/test/*
@@ -16,4 +17,5 @@ echo "cpplintを実行し、コードフォーマットをチェックします"
 cpplint_check $LIB_DIR
 cpplint_check $SAMPLES01_DIR
 cpplint_check $SAMPLES02_DIR
+cpplint_check $SAMPLES03_DIR
 
